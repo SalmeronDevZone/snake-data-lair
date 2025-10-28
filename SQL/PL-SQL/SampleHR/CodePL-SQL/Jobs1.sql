dbd49f80b9f0fe828f02e63c80c1baf9a6c48338
Jesús <162796372+SalmeronDevZone@users.noreply.github.com>
2025-10-28 14:07:23 +0100

    Create log_actividades table and scheduler job

---
+++ SQL/PL-SQL/SampleHR/CodePL-SQL/Jobs1.sql
@@ -0,0 +1,36 @@
+create table log_actividades (
+    id_log int not null,
+    fecha date
+    );
+    
+    
+
+begin
+
+    sys.dbms_scheduler.create_job(
+    
+        job_name => 'job_uno',
+        job_type => 'PLSQL_BLOCK',
+        job_action => declare
+        
+            v_conteo number := 0;
+            
+        begin
+            
+            select count(*) into v_conteo
+            from log_actividades;
+            
+            insert into log_actividades values ( v_conteo + 1, sysdate);
+
+            COMMIT;
+
+        end;
+    
+start_date => systimestamp,
+repeat_interval => 'freq=secondly ; interval = 5';
+end_date => null,
+enabled => true,
+comments => 'log de actividades');
+
+END;
+/
